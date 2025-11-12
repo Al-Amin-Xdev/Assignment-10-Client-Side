180d05ff75c676d53204afc222679a3bdd7d09fb
Md. Al-Amin Mia <md.alamin.mia.webdev@gmail.com>
2025-11-12 20:32:34 +0600

    18th

--- src/AuthProvider/firebase.init.js
+++ src/AuthProvider/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC20EY__ZmwDqj-5wCnFr6Z2DRuxgPeFLk",
-  authDomain: "back-end-authentication-70d55.firebaseapp.com",
-  projectId: "back-end-authentication-70d55",
-  storageBucket: "back-end-authentication-70d55.firebasestorage.app",
-  messagingSenderId: "148860804155",
-  appId: "1:148860804155:web:8dd0fbe4a3dcbf47bb501e"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
